9dffd588c5871d9cbba746700f508ce8daf20fec
alexandragial <160529678+alexandragial@users.noreply.github.com>
2024-03-29 21:55:29 +0200

    Update txt_to_mps_parser.py

--- MPS Files Parser/txt_to_mps_parser.py
+++ MPS Files Parser/txt_to_mps_parser.py
@@ -38,85 +38,88 @@ def writeMps(mpFilename, A, b, c, varName, constType, constName):
 
 
 #--- MAIN ---
-
-print("Convert a text file to an mps file format")
-txFilename = input("Give the name of the text file you want to convert: ")
-mpFilename = input("Give the name of MPS file with .mps extension: ")
-
-with open(txFilename, 'r') as lp:
-
-    line = lp.readline()
-    arr = [] #the elements of this array are the lines of text file as strings
-    while (line[:1] != 'b'):
-        elemArr = line[1:].strip()
-
-        if elemArr != '':
-            elemArr = elemArr.lstrip("=[")
-            elemArr = elemArr.strip("]")
-            elemArr = elemArr.replace('\t', ' ')
-            arr.append(elemArr)
+def main():
+    print("Convert a text file to an mps file format")
+    txFilename = input("Give the name of the text file you want to convert: ")
+    mpFilename = input("Give the name of MPS file with .mps extension: ")
+    
+    with open(txFilename, 'r') as lp:
+    
         line = lp.readline()
+        arr = [] #the elements of this array are the lines of text file as strings
+        while (line[:1] != 'b'):
+            elemArr = line[1:].strip()
+    
+            if elemArr != '':
+                elemArr = elemArr.lstrip("=[")
+                elemArr = elemArr.strip("]")
+                elemArr = elemArr.replace('\t', ' ')
+                arr.append(elemArr)
+            line = lp.readline()
+    
+        stArr = [] # a list of lists with elements each number of the line as STRING
+        for i in range(len(arr)):
+            num = arr[i].split()
+            stArr.append(num)
+    
+        stArr = [[float(string) for string in lists] for lists in stArr] # a list of lists with elements each number of the line as FLOAT
+    
+        npArr = np.array(stArr)
+        trArr = np.transpose(npArr)
+        trList = trArr.tolist()
+    
+        # RIGHT VALUES OF CONSTRAINTS
+        rightVal = []
+        while (line[:1] != 'c'):
+    
+            value = line[3:].rstrip()
+            if value != '':
+                value = value.strip("]")
+                rightVal.append(value)
+            line = lp.readline()
+        rightVal = [float(num) for num in rightVal ]
+    
+        # COEFFICIENTS OF VARIABLES IN OBJECTIVE FUNCTION
+        coefObj = []
+    
+        while (line[:4] != 'Eqin'):
+    
+            coef = line[3:].rstrip()
+            if coef != '':
+                coef = coef.strip("]")
+                coefObj.append(coef)
+            line = lp.readline()
+    
+        coefObj = [float(num) for num in coefObj ]
+    
+        # LIST OF THE NAME OF VARIABLES
+        nameVar = []
+        for i in range(len(coefObj)):
+            nameVar.append("X" + str(i))
+    
+        # TYPE OF CONSTRAINTS
+        typeConst = []
+    
+        while(line[:6] != 'MinMax'):
+            type = line[3:].rstrip()
+    
+            if type != '':
+                type = type.lstrip("n=[")
+                type = type.strip("]")
+                if (type == '-1'):
+                    typeConst.append('L')
+                elif (type == '1' ):
+                    typeConst.append('G')
+                else:
+                    typeConst.append('E')
+            line = lp.readline()
+    
+        # LIST OF THE LABELS OF CONSTRAINTS
+        nameConst = []
+        for i in range(len(typeConst)):
+            nameConst.append("C" + str(i))
+    
+    writeMps(mpFilename, trList, rightVal, coefObj, nameVar, typeConst, nameConst)
 
-    stArr = [] # a list of lists with elements each number of the line as STRING
-    for i in range(len(arr)):
-        num = arr[i].split()
-        stArr.append(num)
-
-    stArr = [[float(string) for string in lists] for lists in stArr] # a list of lists with elements each number of the line as FLOAT
-
-    npArr = np.array(stArr)
-    trArr = np.transpose(npArr)
-    trList = trArr.tolist()
-
-    # RIGHT VALUES OF CONSTRAINTS
-    rightVal = []
-    while (line[:1] != 'c'):
-
-        value = line[3:].rstrip()
-        if value != '':
-            value = value.strip("]")
-            rightVal.append(value)
-        line = lp.readline()
-    rightVal = [float(num) for num in rightVal ]
-
-    # COEFFICIENTS OF VARIABLES IN OBJECTIVE FUNCTION
-    coefObj = []
-
-    while (line[:4] != 'Eqin'):
-
-        coef = line[3:].rstrip()
-        if coef != '':
-            coef = coef.strip("]")
-            coefObj.append(coef)
-        line = lp.readline()
-
-    coefObj = [float(num) for num in coefObj ]
-
-    # LIST OF THE NAME OF VARIABLES
-    nameVar = []
-    for i in range(len(coefObj)):
-        nameVar.append("X" + str(i))
-
-    # TYPE OF CONSTRAINTS
-    typeConst = []
-
-    while(line[:6] != 'MinMax'):
-        type = line[3:].rstrip()
-
-        if type != '':
-            type = type.lstrip("n=[")
-            type = type.strip("]")
-            if (type == '-1'):
-                typeConst.append('L')
-            elif (type == '1' ):
-                typeConst.append('G')
-            else:
-                typeConst.append('E')
-        line = lp.readline()
-
-    # LIST OF THE LABELS OF CONSTRAINTS
-    nameConst = []
-    for i in range(len(typeConst)):
-        nameConst.append("C" + str(i))
-
-writeMps(mpFilename, trList, rightVal, coefObj, nameVar, typeConst, nameConst)
+if __name__=="__main__":
+  main()
